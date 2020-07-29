6b211e5fc965ee6349935d2762d4dc50f5088cd8
goodpic <kaneko@gmail.com>
2020-07-29 15:07:31 +0100

    firebase 7.5.1+ cause the error

---
+++ src/__tests__/firebase.spec.ts
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+// import '@firebase/auth'
+import { firebaseConfig } from '../../config/firebase'
+
+describe('UseProduct', () => {
+  beforeAll(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig)
+    }
+  })
+
+  test('fetchProduct', async () => {
+    try {
+      const defaultAuth = firebase.auth()
+    } catch (e) {
+      console.log(e)
+    }
+  }, 100000)
+})
