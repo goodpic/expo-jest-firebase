2e40b04788885778570a3bdf22103cdff514115d
goodpic <kaneko@gmail.com>
2020-09-03 13:06:14 +0100

    implement basic validation rules

--- src/components/ValidatedForm/__tests__/validateInput.spec.ts
+++ src/components/ValidatedForm/__tests__/validateInput.spec.ts
@@ -2,15 +2,15 @@ import { validateInput } from '../validateInput'
 
 describe('validateInput', () => {
   const stringInput = {
-    key: "id",
-    label: "User ID",
+    key: 'id',
+    label: 'User ID',
   }
 
   test('valid string', () => {
     const result = validateInput('validstring', {
       ...stringInput,
-      type: "string",
-      validate: "alphanumeric",
+      type: 'string',
+      validate: 'alphanumeric',
     })
     expect(result.isValid).toEqual(true)
   })
@@ -18,8 +18,8 @@ describe('validateInput', () => {
   test('required and filled', () => {
     const result = validateInput('validstring', {
       ...stringInput,
-      type: "string",
-      validate: "alphanumeric",
+      type: 'string',
+      validate: 'alphanumeric',
       required: true,
     })
     expect(result.isValid).toEqual(true)
@@ -28,9 +28,53 @@ describe('validateInput', () => {
   test('required but blank', () => {
     const result = validateInput('', {
       ...stringInput,
-      type: "string",
-      validate: "alphanumeric",
+      type: 'string',
+      validate: 'alphanumeric',
       required: true,
+    })
+    expect(result.isValid).toEqual(false)
+  })
+
+  test('boolean', () => {
+    const result = validateInput('string', {
+      ...stringInput,
+      type: 'boolean',
+    })
+    expect(result.isValid).toEqual(false)
+  })
+
+  test('minLength', () => {
+    const result = validateInput('aaa', {
+      ...stringInput,
+      type: 'string',
+      minLength: 5
+    })
+    expect(result.isValid).toEqual(false)
+  })
+
+  test('maxLength', () => {
+    const result = validateInput('aaaa', {
+      ...stringInput,
+      type: 'string',
+      maxLength: 3
+    })
+    expect(result.isValid).toEqual(false)
+  })
+
+  test('alphabet', () => {
+    const result = validateInput('aaaa', {
+      ...stringInput,
+      type: 'string',
+      validate: 'alphabet',
+    })
+    expect(result.isValid).toEqual(true)
+  })
+
+  test('must be alphabet but contains space', () => {
+    const result = validateInput(' aaaa', {
+      ...stringInput,
+      type: 'string',
+      validate: 'alphabet',
     })
     expect(result.isValid).toEqual(false)
   })
--- src/components/ValidatedForm/validateInput.ts
+++ src/components/ValidatedForm/validateInput.ts
@@ -7,13 +7,41 @@ function validateInput(value: string | number | boolean, input: InputType): {
 } {
   if (input.required && (value === '' || !value)) return {
     isValid: false,
-    error: 'Required',
+    error: 'required',
     value,
   }
+  if (input.type === 'boolean' && typeof value !== 'boolean') return {
+    isValid: false,
+    error: 'boolean',
+    value,
+  }
+  let parsed = value
+  if (input.type === 'string' && typeof value !== 'string') parsed = `${value}`
+  if (input.type === 'number' && typeof value === 'string') parsed = parseInt(value)
+  if (typeof parsed === 'string'){
+    if (input.minLength && parsed.length < input.minLength) return {
+      isValid: false,
+      error: `more than ${input.minLength} characters`,
+      value
+    }
+    if (input.maxLength && parsed.length > input.maxLength) return {
+      isValid: false,
+      error: `less than ${input.minLength} characters`,
+      value
+    }
+    if (input.validate) {
+      if (input.validate === 'alphabet' && parsed.match(/[^a-bA-B]+/)) return {
+        isValid: false,
+        error: `alphabet`,
+        value
+      }
+    }
+  }
+
   return {
     isValid: true,
     error: '',
-    value,
+    value: parsed,
   }
 }
 
