79080cfdab0df7dcbbc7bb8d3a2b447e69f64eb6
goodpic <kaneko@gmail.com>
2020-09-24 17:07:24 +0100

    update

--- src/styles/Variables.ts
+++ src/styles/Variables.ts
@@ -3,5 +3,14 @@ const Variables = {
     iPad: 768,
     iPhone: 414, //iPhone8 Plus
   },
+  height: {
+    statusBar: 44,
+    navigationBar: 97,
+    tabBar: 49,
+    homeIndicator: 34,
+  },
+  margin: {
+    layout: 16,
+  }
 }
 export { Variables }
